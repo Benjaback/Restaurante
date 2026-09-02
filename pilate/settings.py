77de828d32ac73106dd6a7f0d596993cd0a15ce6
Camii-web <camila.penalva03@gmail.com>
2026-09-02 00:46:23 -0300

    setin

--- pilate/settings.py
+++ pilate/settings.py
@@ -83,8 +83,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'restaurante',
-        'USER': 'benja',
-        'PASSWORD': 'admin123',
+        'USER': 'root',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
